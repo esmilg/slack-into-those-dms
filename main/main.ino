fb6106167be9e450dc7272062fd7eb4fa5a31ce2
Ethan Smilg <ethan.smilg@boxcast.com>
2020-12-16 11:52:53 -0500

    updated project structure and .gitignore

---
+++ main/main.ino
@@ -0,0 +1,18 @@
+#include <ArduinoSlack.h>
+#include <ArduinoSlackCert.h>
+#include <WiFiNINA.h>
+#include <LiquidCrystal.h>
+
+#include "secrets.h" 
+///////please enter your sensitive data in the Secret tab/secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
